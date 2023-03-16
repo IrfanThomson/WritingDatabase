327dbebd439a8d08a5257a338c2e741d5401fa72
Irfan Thomson <irfanthomson@gmail.com>
2023-03-16 10:00:53 -0500

    db change

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app)
 app.debug=True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:zerobugfirsttry@writingdb-instance-1.cpa1c490xrlk.us-east-1.rds.amazonaws.com'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:zerobugfirsttry@writingdatabase.cpa1c490xrlk.us-east-1.rds.amazonaws.com'
 db = SQLAlchemy(app)
 
 class Story(db.Model):
